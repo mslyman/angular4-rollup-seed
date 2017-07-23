f9ba16e2668e34a1569f4e3e648e3fb4db727034
mslyman <lymanlyman12@gmail.com>
2017-07-23 18:47:29 +0300

    del unnesessary js

--- js/bare-spa/main-deploy.js
+++
@@ -1,8 +0,0 @@
-import { platformBrowser } from '@angular/platform-browser';
-import { AppModuleNgFactory } from '../../aot/js/bare-spa/app/app.module.ngfactory';
-import { enableProdMode } from '@angular/core';
-//window['module2'] = window['module2'] || {}; // For using @Component({moduleId: module.id,
-enableProdMode();
-//console.log('Running AOT compiled');
-//console.log(window['module2']);
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
